b783bf2d8ca3a2ec92c3d1e3cbc3dd5b75c49699
root <root@localhost.localdomain>
2024-02-25 21:55:25 -0400

    agregue comentario

--- src/main/java/Administrator.java
+++ src/main/java/Administrator.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
-public class Administrator extends People {
+public class Administrator extends People implements IOperationAdministrator {
     // Constructor
+    // Este es un comentario
     public Administrator(String bankName, String address, String contact,
                          ArrayList<Customer> clients, ArrayList<Loan> loans,
                          ArrayList<SavingsAccount> savingsAccount, ArrayList<CurrentAccount> currentsAccount,
@@ -13,5 +14,50 @@ public class Administrator extends People {
     public void performAdministratorOperation() {
         // Implementa la lógica específica para operaciones de administrador
         System.out.println("Performing administrator operation");
+    }
+
+    @Override
+    public void registerCustomers() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("\nIndique su nombre:");
+        String name = entrada.nextLine();
+        System.out.println("\nIndique su apellido:");
+        String lastName = entrada.nextLine();
+        System.out.println("\nIndique su ID:");
+        String id = entrada.nextLine();
+        System.out.println("\nIndique su direccion:");
+        String address = entrada.nextLine();
+        System.out.println("\nIndique su numero de contacto:");
+        String contact = entrada.nextLine();
+    }
+
+    @Override
+    public void removeCustomers() {
+
+    }
+
+    @Override
+    public void createSavingsAccount() {
+
+    }
+
+    @Override
+    public void createCurrentAccount() {
+
+    }
+
+    @Override
+    public void createDebitCard() {
+
+    }
+
+    @Override
+    public void createCreditCard() {
+
+    }
+
+    @Override
+    public void createLoan() {
+
     }
 }
